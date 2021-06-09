15d4701d500a567468be8ba9d163b3e310e57984
Jeremy Bar <78321429+jeremybar32093@users.noreply.github.com>
2021-06-08 20:25:01 -0400

    Updated scrape_mars.py to scrape table from mars facts site, updated index.html to display mars facts table

--- missions_to_mars/scrape_mars.py
+++ missions_to_mars/scrape_mars.py
@@ -30,10 +30,59 @@ def scrape_info():
     latest_article_paragraph = article_paragraphs[0].text
     # -------------------------------------
 
+    # ----- 2.) Scrape Mars featured image site -----
+    # Use splinter browser variable to navigate to the mars news site
+    url = 'https://spaceimages-mars.com/'
+    browser.visit(url)
+    time.sleep(1)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+    # Use div with class 'content_title' to find list of all news titles
+    featured_image = soup.find('img', class_='headerimage')
+    # Pull first one to get latest title
+    featured_image_url =  url + featured_image['src']
+    # -------------------------------------
+
+    # ----- 3.) Use pandas to scrape Mars Facts table -----
+    # Define url for pandas to scrape
+    url = 'https://galaxyfacts-mars.com/'
+    # Read in tables from url defined above
+    tables = pd.read_html(url)
+    # Convert list returned above into dataframe
+    table_df = tables[0]
+    # Rename columns and reset index
+    table_df.columns = table_df.iloc[0]
+    table_df = table_df.drop(table_df.index[0])
+    # Output scraped dataframe to html string
+    table_html_string = table_df.to_html(index=False).replace('\n', '')
+    # Replace table tag with bootstrap class rather than generated html from pandas
+    table_html_string = table_html_string.replace('<table border="1" class="dataframe">','<table class="table table-striped">')
+    # -------------------------------------
+
+    # ----- 3.) Scrape Mars hemispheres site and pull full resolution image paths -----
+    # Define url for pandas to scrape
+    url = 'https://galaxyfacts-mars.com/'
+    # Read in tables from url defined above
+    tables = pd.read_html(url)
+    # Convert list returned above into dataframe
+    table_df = tables[0]
+    # Rename columns and reset index
+    table_df.columns = table_df.iloc[0]
+    table_df = table_df.drop(table_df.index[0])
+    # Output scraped dataframe to html string
+    table_html_string = table_df.to_html(index=False).replace('\n', '')
+    # Replace table tag with bootstrap class rather than generated html from pandas
+    table_html_string = table_html_string.replace('<table border="1" class="dataframe">','<table class="table table-striped">')
+    # -------------------------------------
+
     # Store data in a dictionary
     mars_data = {
         "latest_article_title": latest_article_title,
         "latest_article_paragraph": latest_article_paragraph,
+        "featured_image_url":featured_image_url,
+        "table_html_string":table_html_string,
     }
 
     # Close the browser after scraping
