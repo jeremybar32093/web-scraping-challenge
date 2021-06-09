03c1207a678589b77b24cabe760c3eaad5c8f775
Jeremy Bar <78321429+jeremybar32093@users.noreply.github.com>
2021-06-08 20:00:33 -0400

    Updated scrape_mars.py to scrape mars news site and store in MongoDB, updated app.py index.html to retrieve values from MongoDB database mars_db

--- missions_to_mars/scrape_mars.py
+++ missions_to_mars/scrape_mars.py
@@ -1,46 +1,43 @@
+# Import dependencies
 from splinter import Browser
-from bs4 import BeautifulSoup as bs
-import time
+from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import time
 
-
+# Define scrape function
 def scrape_info():
     # Set up Splinter
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
 
-    # Visit visitcostarica.herokuapp.com
-    url = "https://visitcostarica.herokuapp.com/"
+    # ----- 1.) Scrape Mars news site -----
+    # Use splinter browser variable to navigate to the mars news site
+    url = 'https://redplanetscience.com/'
     browser.visit(url)
-
     time.sleep(1)
 
     # Scrape page into Soup
     html = browser.html
-    soup = bs(html, "html.parser")
-
-    # Get the average temps
-    avg_temps = soup.find('div', id='weather')
-
-    # Get the min avg temp
-    min_temp = avg_temps.find_all('strong')[0].text
-
-    # Get the max avg temp
-    max_temp = avg_temps.find_all('strong')[1].text
-
-    # BONUS: Find the src for the sloth image
-    relative_image_path = soup.find_all('img')[2]["src"]
-    sloth_img = url + relative_image_path
+    soup = BeautifulSoup(html, "html.parser")
+    # Use div with class 'content_title' to find list of all news titles
+    article_titles = soup.find_all('div', class_='content_title')
+    # Pull first one to get latest title
+    latest_article_title = article_titles[0].text
+    # Use div with class 'article_teaser_body' to find list of news paragraph text
+    article_paragraphs = soup.find_all('div', class_='article_teaser_body')
+    # Pull first one to get latest article paragraph
+    latest_article_paragraph = article_paragraphs[0].text
+    # -------------------------------------
 
     # Store data in a dictionary
-    costa_data = {
-        "sloth_img": sloth_img,
-        "min_temp": min_temp,
-        "max_temp": max_temp
+    mars_data = {
+        "latest_article_title": latest_article_title,
+        "latest_article_paragraph": latest_article_paragraph,
     }
 
     # Close the browser after scraping
     browser.quit()
 
     # Return results
-    return costa_data
+    return mars_data
